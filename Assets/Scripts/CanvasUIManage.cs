278fcce19347a94ef775a0d9c8deb6e0e7fc92dd
KtoK21 <rlgus01003@gmail.com>
2019-02-17 00:12:09 +0900

    Initial commit

---
+++ Assets/Scripts/CanvasUIManage.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CanvasUIManage : MonoBehaviour
+{
+
+    public void ExitGameOnClick()
+    {
+        Application.Quit();
+    }
+
+    public void SceneLoadOnClick(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void ObjectToggleOnClick(GameObject obj)
+    {
+        obj.SetActive(!obj.activeSelf);
+    }
+
+    public void OnEndEdit(string text)
+    {
+        GameObject.Find("Manager").GetComponent<ProfileManage>().ChangeProfileName(text);
+    }
+
+    public void SaveProfileOnClick()
+    {
+        GameObject.Find("Manager").GetComponent<ProfileManage>().SaveProfiletoJson();
+    }
+
+    public void LoadProfileOnClick()
+    {
+        GameObject.Find("Manager").GetComponent<ProfileManage>().LoadProfilefromJson();
+    }
+}
---
+++ Assets/Scripts/ProfileManage.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[SerializeField]
+public class Profile
+{
+    public string Name;
+}
+
+public class ProfileManage : MonoBehaviour
+{
+    Profile _Profile;
+    private string dataPath;
+    private void Awake()
+    {
+        _Profile = new Profile();
+        dataPath = Application.dataPath + "Profile.json";
+    }
+
+    public void ChangeProfileName(string name)
+    {
+        _Profile.Name = name;
+        Debug.Log(_Profile.Name);
+    }
+
+    public void SaveProfiletoJson()
+    {
+        string saveString = JsonUtility.ToJson(_Profile, prettyPrint: true);
+
+        File.WriteAllText(dataPath, saveString);
+    }
+
+    public void LoadProfilefromJson()
+    {
+
+        _Profile = JsonUtility.FromJson<Profile>(File.ReadAllText(dataPath));
+        Debug.Log(_Profile.Name);
+    }
+}
